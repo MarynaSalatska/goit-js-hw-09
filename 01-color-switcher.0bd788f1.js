3f9400f9cc5a4f7bd112581eb5e554cacc3f7e73
MarynaSalatska <yasinskamarina@gmail.com>
2023-01-05 15:52:14 +0000

    Deploying to gh-pages from @ MarynaSalatska/goit-js-hw-09@f60ec227b57ee1c5263d94e4ed112f921425a1ee 🚀

---
+++ 01-color-switcher.0bd788f1.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.disabled=!0;t.addEventListener("click",(function(){return void setInterval((function(){}),1e3)})),e.addEventListener("click",(function(){}))}();
+//# sourceMappingURL=01-color-switcher.0bd788f1.js.map
---
+++ 01-color-switcher.209117e6.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let n=null;t.disabled=!0;e.addEventListener("click",(function(){return void(n=setInterval((()=>{}),1e3))})),t.addEventListener("click",(function(){}));
+//# sourceMappingURL=01-color-switcher.209117e6.js.map
