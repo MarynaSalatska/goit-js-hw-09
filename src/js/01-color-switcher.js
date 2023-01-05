f60ec227b57ee1c5263d94e4ed112f921425a1ee
Maryna <yasinskamarina@gmail.com>
2023-01-05 17:51:50 +0200

    main

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+stopBtn.disabled = true;
+const INTERVAL_DELAY = 1000;
 
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+  timerId = setInterval(() => {}, INTERVAL_DELAY);
+  return;
+}
+
+function onStopBtn() {}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
